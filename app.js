4e3b92965731e58b262497b8fc715949ecb0f102
Ratul-728 <s.m.alam@northsouth.edu>
2020-08-08 18:45:31 +0600

    Third

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "1234"))
 var session = driver.session()
 
 
-
-
 // Home page
 app.get('/', function(req, res){
     session
@@ -38,7 +36,8 @@ app.get('/', function(req, res){
                     name: record._fields[0].properties.name
                 });
 
-            });
+            })
+
 
             session
                 .run("MATCH (d: Disease) RETURN d")
@@ -51,63 +50,31 @@ app.get('/', function(req, res){
                             name: record._fields[0].properties.name
                         });
         
-                    });
-                    res.render('index', {
-                        disease : dArr,
-                        treatment : tArr
-                    });
+                    })
+
+                    session
+                        .run("MATCH (s: Symtom) RETURN s")
+                        .then(function(result3){
+                            var sArr = [];
+                            result3.records.forEach(function(record){
+                                sArr.push({
+                                    id: record._fields[0].identity.low,
+                                    name: record._fields[0].properties.name
+                                });
+                            })
+
+                            res.render('index', {
+                                disease: dArr,
+                                treatment: tArr,
+                                symtom: sArr
+                            });
+                        })
                 })
         })
         .catch(function(error){
             console.log(error)
         });
 
-});
-
-//Add Treatment
-app.post('/treatment/add', function(req, res){
-    var name = req.body.name;
-    
-    session
-        .run("CREATE(n:Treatment {name:{nameParam}}) RETURN n.name", {nameParam: name})
-        .then(function(result){
-            res.redirect('/');
-            //session.close();
-        })
-        .catch(function(error){
-            console.log(error);
-        });
-});
-
-//Add Disease
-app.post('/disease/add', function(req, res){
-    var dname = req.body.name;
-    
-    session
-        .run("CREATE(n:Disease {name:{nameParam}}) RETURN n.name", {nameParam: dname})
-        .then(function(result){
-            res.redirect('/');
-            //session.close();
-        })
-        .catch(function(error){
-            console.log(error);
-        });
-});
-
-//Add Relationship
-app.post('/relationship/add', function(req, res){
-    var name1 = req.body.name1;
-    var name2 = req.body.name2;
-    
-    session
-        .run("match (d:Disease {name: {nameParam1}}) , (t:Treatment {name: {nameParam2}}) merge(d)-[r:Treated_By]->(t)", {nameParam1: name1, nameParam2: name2})
-        .then(function(result){
-            res.redirect('/');
-            //session.close();
-        })
-        .catch(function(error){
-            console.log(error);
-        });
 });
 
 // Disease Details
@@ -145,6 +112,91 @@ app.get('/disease/:id', function(req, res){
             console.log(error)
         });
 });
+
+// New Add Disease
+app.get('/add', function(req, res){
+    res.render('AddDisease')
+});
+
+app.post('/disease/add', function(req, res){
+    var dname = req.body.name;
+    var ddescription = req.body.description;
+    session
+        .run("CREATE(n:Disease {name:{nameParam}, description:{desParam}})", {nameParam: dname, desParam: ddescription})
+        .then(function(result){
+            res.render('Success');
+        });
+});
+
+// New Add Treatment
+app.get('/addT', function(req, res){
+    res.render('AddTreatment')
+});
+
+app.post('/treatment/add', function(req, res){
+    var tname = req.body.name;
+    session
+        .run("CREATE(n:Treatment {name:{tnameParam}})", {tnameParam: tname})
+        .then(function(result){
+            res.render('Success');
+        });
+});
+
+// New Add Symtoms
+app.get('/addS', function(req, res){
+    res.render('AddSymtoms')
+});
+
+app.post('/symtoms/add', function(req, res){
+    var sname = req.body.name;
+    //console.log(sname);
+    session
+        .run("CREATE(s:Symtom {name:{snameParam}})", {snameParam: sname})
+        .then(function(result){
+            res.render('Success');
+        });
+});
+
+//Add Relationship Between Disease and Treatment
+app.get('/addRT', function(req,res){
+    res.render('AddRT')
+});
+
+app.post('/relationship/addT', function(req, res){
+    var name1 = req.body.name1;
+    var name2 = req.body.name2;
+    
+    session
+        .run("match (d:Disease {name: {nameParam1}}) , (t:Treatment {name: {nameParam2}}) merge(d)-[r:Treated_By]->(t)", {nameParam1: name1, nameParam2: name2})
+        .then(function(result){
+            res.render('Success');
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+});
+
+//Add Relationship Between Disease and Symtom
+app.get('/addRS', function(req,res){
+    res.render('AddRS')
+});
+
+app.post('/relationship/addS', function(req, res){
+    var name1 = req.body.name1;
+    var name2 = req.body.name2;
+    
+    session
+        .run("match (d:Disease {name: {nameParam1}}) , (s:Symtom {name: {nameParam2}}) merge(d)-[r:Symtom]->(s)", {nameParam1: name1, nameParam2: name2})
+        .then(function(result){
+            res.render('Success')
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+});
+
 
 app.listen(3000);
 
